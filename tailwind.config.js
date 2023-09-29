75e1da27369f0d0066d8ce3f5fbc423242ab48a8
shubham Nema <shubham.n@simformsolutions.com>
2023-09-29 18:53:13 +0530

    [refactor] create optimization.card,banner page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    
+    screens: {
+      // '2xl': { 'max': '1280px' },
+      // 'xl': { 'max': '1280px' },
+      // 'lg': { 'max': '1023px' },
+      // 'md': { 'max': '767px' },
+      // 'sm': { 'max': '639px' },
+      // 'xs': { 'max': '365px' },
+
+      xs:'365px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1280px',
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['PT Sans, sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
