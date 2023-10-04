38b62290ee95163dfd862a010ca7a1c31aabe1fa
shubham Nema <shubham.n@simformsolutions.com>
2023-10-04 18:04:49 +0530

    add customized dark themeing and header responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    
     screens: {
       // '2xl': { 'max': '1280px' },
       // 'xl': { 'max': '1280px' },
@@ -13,7 +12,7 @@ module.exports = {
       // 'sm': { 'max': '639px' },
       // 'xs': { 'max': '365px' },
 
-      xs:'345px',
+      xs: '345px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
@@ -28,10 +27,22 @@ module.exports = {
         'blue-hover': 'rgba(3, 121, 182, 0.4)',
         'footer-bg': '#003048;',
         'footer-font-color': '#0379B6',
-        'dark-blue': '#001B29'
+        'dark-blue': '#001B29',
+        'click-me': '#0000FF',
+        dark: {
+          'click-me': '#000000',
+        },
+      },
+      animation: {
+        smoothScroll: 'smoothScroll 0.5s forwards;'
+      },
+      keyframes: {
+        smoothScroll: {
+          '0%': { transform: 'translateY(-40px)' },
+          '100%': { transform: ' translateY(0px)' },
+        },
       },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
-
